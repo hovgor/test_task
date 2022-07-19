b2d781543f024825c6f32ac273b548d7fc0fc35e
Gor <gor@kreplex.com>
2022-07-19 04:42:26 +0400

    fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,21 +2,28 @@ import {
   BadRequestException,
   Body,
   Controller,
+  Delete,
+  Get,
   HttpStatus,
   NotFoundException,
+  Param,
+  ParseIntPipe,
   Post,
+  Req,
   Res,
+  UnauthorizedException,
   UnprocessableEntityException,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { Response } from 'express';
 import { RealIP } from 'nestjs-real-ip';
 import { AuthService } from 'src/auth/auth.service';
 import { sendEmail } from 'src/send-email/sendEmail';
 import { HashPassword } from 'src/shared/password_hash/passwordHash';
+import { UserRoles } from 'src/types/roles';
 import { UserValidator } from 'src/validaters/UserValidator';
 import { UserLoginDto } from './dto/userLoginDto';
 import { UserRegisterDto } from './dto/userRegisterDto';
@@ -104,6 +111,117 @@ export class UsersController {
       await this.userService.insertToken(token, user.id);
       return res.status(HttpStatus.OK).json({
         access_token: token,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @ApiBearerAuth()
+  @Get('all')
+  async getAllUsers(@Res() res: Response, @Req() req) {
+    try {
+      const token = (req.headers['authorization'] + '').split(' ')[1];
+      if (!token) {
+        throw new NotFoundException('Token does not exist!!!');
+      }
+      const tokenId = await this.authService.afterDecode(token);
+      if (!tokenId) {
+        throw new BadRequestException('Something went wrong!!!');
+      }
+      const user = await this.userService.getUserById(tokenId);
+      if (user.role !== UserRoles.Admin) {
+        throw new UnauthorizedException('User is not authorized!!!');
+      }
+      const allUsers = await this.userService.getAllUsers();
+      if (!allUsers) {
+        throw new UnprocessableEntityException('Users is not exist!!!');
+      }
+      res.status(HttpStatus.OK).json(allUsers);
+    } catch (error) {
+      throw error;
+    }
+  }
+  //logout
+  @UsePipes(new ValidationPipe())
+  @ApiBearerAuth()
+  @Get('logout')
+  async logout(@Res() res: Response, @Req() req) {
+    try {
+      const token = req.headers.authorization.replace('Bearer ', '');
+      if (!token) {
+        throw new NotFoundException('Token does not exist !!!');
+      }
+      const decId = await this.authService.afterDecode(token);
+      if (!decId) {
+        throw new NotFoundException('Decode id does not exist !!!');
+      }
+      const user = await this.userService.getUserById(decId);
+      if (user.token !== token) {
+        throw new BadRequestException('Token is removed!!!');
+      }
+      if (!user.token || !user) {
+        throw new BadRequestException('User is not defined!!!');
+      }
+      await this.userService.deleteToken(user.id);
+      return res.status(HttpStatus.RESET_CONTENT).json({
+        success: true,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+  // get by id
+  @UsePipes(new ValidationPipe())
+  @ApiBearerAuth()
+  @Get(':id')
+  async getTask(@Req() req, @Param('id') id: number, @Res() res: Response) {
+    try {
+      const token = (req.headers['authorization'] + '').split(' ')[1];
+      if (!token) {
+        throw new UnauthorizedException('User is not unauthorized!!!');
+      }
+      const payload = await this.authService.decodeToken(token);
+      const user = await this.authService.validateUser(payload, token);
+
+      if (!(await this.authService.adminVerify(user))) {
+        throw new UnauthorizedException('User is not unauthorized!!!');
+      }
+      const userGet = await this.userService.getUserById(id);
+      if (!userGet) {
+        throw new BadRequestException('user is not exist!!!');
+      }
+      return res.status(HttpStatus.OK).json(userGet);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // delete user
+  @UsePipes(new ValidationPipe())
+  @ApiBearerAuth()
+  @Delete('/:id')
+  async deleteUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response,
+    @Req() req,
+  ) {
+    try {
+      const token = req.headers.authorization.replace('Bearer ', '');
+
+      if (!token) {
+        throw new UnauthorizedException('user is not authorized');
+      }
+      if (!(await this.authService.adminVerify(token))) {
+        throw new UnauthorizedException('Admin not authorized !!!');
+      }
+      const user = await this.userService.getUserById(id);
+      if (!user) {
+        throw new NotFoundException('User is not found !');
+      }
+      await this.userService.deleteUser(id);
+      return res.status(HttpStatus.NO_CONTENT).json({
+        success: true,
       });
     } catch (error) {
       throw error;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,6 +21,37 @@ export class UsersService {
     }
   }
 
+  // get user by id
+  async getUserById(id: number) {
+    try {
+      return await this.usersRepository.findOne({ where: { id: id } });
+    } catch (error) {
+      Logger.log('error: get user by id =>', error);
+      throw error;
+    }
+  }
+
+  // get all users
+  async getAllUsers() {
+    try {
+      return await this.usersRepository.find();
+    } catch (error) {
+      Logger.log('error: get all users => ', error);
+      throw error;
+    }
+  }
+
+  // update user
+  async updateUser(id: number, data: any) {
+    try {
+      return await this.usersRepository.update({ id }, data);
+    } catch (error) {
+      Logger.log('error: update user =>', error);
+      throw error;
+    }
+  }
+
+  // get user by email
   async getUserByEmail(data: string) {
     try {
       return await this.usersRepository.findOne({ where: { email: data } });
@@ -38,6 +69,28 @@ export class UsersService {
       return await this.usersRepository.save(user);
     } catch (error) {
       Logger.log('error: insert token in db => ', error);
+      throw error;
+    }
+  }
+
+  // delete user
+  async deleteUser(id: number) {
+    try {
+      await this.usersRepository.delete(id);
+    } catch (error) {
+      Logger.log('error: delete user by id => ', error);
+      throw error;
+    }
+  }
+
+  // delete token in db
+  async deleteToken(id: number) {
+    try {
+      const user = await this.usersRepository.findOne({ where: { id } });
+      user.token = null;
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      Logger.log('error: delete token => ', error);
       throw error;
     }
   }
